0d55a1d85275d59d70231ac5b95d4ec23f19d755
David Yang <mmyangfl@gmail.com>
2025-09-14 02:19:28 +0800

    qualcommax: ipq50xx: add support for Xiaomi CR8818
    This is a low-end variant of Xiaomi AX6000 (commit d7f9e24).
    
    Specification
    =============
    * SoC: Qualcomm IPQ5018 (64-bit dual-core ARM Cortex-A53 @ 1.0Ghz)
    * RAM: 256 MiB
    * Flash: Either of:
      - Gigadevice GD5F1GQ4RE9IGD (128 MiB)
      - ESMT F50D1G41LB (128 MiB)
    * Wi-Fi:
      - IPQ5018 (2x2 2.4 Ghz 802.11b/g/n/ax - up to 574 Mbps)
      - QCN6122 (2x2 5 Ghz 802.11an/ac/ax - up to 2402 Mbps)
    * Ethernet: IPQ5018 integrated virtual switch connected to:
      - external YT9215S switch   (3 LAN + 1 WAN Ports 10/100/1000)
    * LEDs:
      - System Blue   (GPIO 24 Active High)
      - System Red    (GPIO 25 Active High)
      - Internet Blue (GPIO 26 Active High)
      - Internet Red  (GPIO 27 Active High)
    * Buttons:
      - Reset (GPIO 38 Active Low)
      - Mesh  (GPIO 28 Active Low)
    * UART: 1v8 TTL 115200n8
    
    CAUTION
    =======
    This model might be labeled as CR8808, and may be referred as CR8808
    Ver.B in some online forums. However, the two models are different and
    flashing incompatible firmwares might result in soft brick and requires
    TFTP recovery.
    
    You may check the real model by:
    * Run `nvram get model` in Linux or `printenv model` in U-Boot, you
      should get `CR8818`.
    * Go to the web interface of the vendor firmware and search in the source:
      `hardware = 'cr8818';`
    
      Do not trust the web footnote.
    * PCB SN: `M79 R0205 20230713`
    
    Known Issues
    ============
    * All Wi-Fi related peripherals are disabled.
    
      This device has only 256 MiB RAM and it's too few for ath11k. To
      prevent OOM when using LuCI or other softwares, disable Wi-Fi related
      peripherals in device tree at the moment.
    * YT9215S supports 2500Base-X, but I cannot make IPQ5018 work with it.
      It is limited to SGMII for now.
    
    Installation
    ============
    Please follow instructions for Xiaomi AX6000 (commit d7f9e24).
    
    Signed-off-by: David Yang <mmyangfl@gmail.com>

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -189,6 +189,13 @@ endif
 endef
 TARGET_DEVICES += xiaomi_ax6000
 
+define Device/xiaomi_cr8818
+	$(call Device/xiaomi_ipq50xx_base)
+	DEVICE_MODEL := CR8818
+	DEVICE_DTS_CONFIG := config@mp03.3
+endef
+TARGET_DEVICES += xiaomi_cr8818
+
 define Device/yuncore_ax830
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
